e918bad2e87d862c56bdd7da4d9fbd5f7ab8fc87
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2023-12-28 15:00:24 +0000

    start on groups solutions

--- FormalisingMathematics2024/Solutions/Section04groups/Sheet1.lean
+++ FormalisingMathematics2024/Solutions/Section04groups/Sheet1.lean
@@ -3,11 +3,8 @@ Copyright (c) 2023 Kevin Buzzard. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Author : Kevin Buzzard
 -/
-import Mathlib.Tactic.Default
+import Mathlib.Tactic -- imports all the Lean tactics
 
-
--- imports all the Lean tactics
--- imports all the Lean tactics
 /-
 
 # Groups
@@ -36,36 +33,35 @@ groups, rings, whatever: what's the point if these things are there already?
 So in this sheet I explain how to use Lean's groups.
 
 -/
--- let G be a group
+
 -- let G be a group
 variable (G : Type) [Group G]
 
--- Let's see what just happened.
 example (g : G) : g⁻¹ * g = 1 :=
-  /- The tactic state now looks like this:
-      
-      G : Type
-      _inst_1 : group G
-      g : G
-      ⊢ g⁻¹ * g = 1
-      
-      So G is what most mathematicians would call a "set", and what in this course
-      we call a "type" (they're the same thing as far as you're concerned), and
-      `g : G` just mean "g is an element of G". The remaining thing is this 
-      `_inst_1` thing, and that means "G has a multiplication `*`, an identity `1`,
-      an inverse function `⁻¹`, and satisfies all the group axioms; furthermore
-      all of this will be taken care of by "instances", which are a part
-      of Lean's "type class inference system". The type class inference system
-      is the system which deals with stuff in square brackets. You don't have
-      to worry about it right now -- all that matters is that you have access
-      to all the group axioms. This one is called `inv_mul_self g`. 
-      -/
-    inv_mul_self
-    g
+/-  Let's see what just happened.
+    The tactic state now looks like this:
 
--- Why don't you use `library_search` to see the names of the other axioms
--- of a group? Note that when `library_search` has run, you can click on
--- the output (the blue output in the infoview) and replace `library_search`
+    G : Type
+    inst✝ : Group G
+    g : G
+    ⊢ g⁻¹ * g = 1
+
+    So G is what most mathematicians would call a "set", and what in this course
+    we call a "type" (they're the same thing as far as you're concerned), and
+    `g : G` just mean "g is an element of G". The remaining thing is this
+    `inst✝` thing, and that means "G has a multiplication `*`, an identity `1`,
+    an inverse function `⁻¹`, and satisfies all the group axioms; furthermore
+    all of this will be taken care of by "instances", which are a part
+    of Lean's "type class inference system". The type class inference system
+    is the system which deals with stuff in square brackets. You don't have
+    to worry about it right now -- all that matters is that you have access
+    to all the group axioms. This one is called `inv_mul_self g`.
+-/
+  inv_mul_self g
+
+-- Why don't you use `exact?` to see the names of the other axioms
+-- of a group? Note that when `exact?` has run, you can click on
+-- the output (the blue output in the infoview) and replace `exact?`
 -- with the name of the axiom it found. Note also that you can instead *guess*
 -- the names of the axioms. For example what do you think the proof of `1 * a = a` is called?
 example (a b c : G) : a * b * c = a * (b * c) :=
@@ -98,7 +94,7 @@ example : a * (a⁻¹ * b) = b := by rw [← mul_assoc, mul_inv_self, one_mul]
 example {a b c : G} (h1 : b * a = 1) (h2 : a * c = 1) : b = c :=
   by
   have h : b * (a * c) = b * a * c := by rw [mul_assoc]
-  rwa [h1, h2, mul_one, one_mul] at h 
+  rwa [h1, h2, mul_one, one_mul] at h
 
 -- mul_eq_one_iff_inv_eq
 example : a * b = 1 ↔ a⁻¹ = b := by
@@ -145,4 +141,3 @@ example (h : ∀ g : G, g * g = 1) : ∀ g h : G, g * h = h * g :=
     exact h g
   intro g h
   rw [useful (g * h), mul_inv_rev, ← useful g, ← useful h]
-
