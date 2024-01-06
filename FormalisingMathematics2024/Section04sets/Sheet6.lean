01eecbe414a91b17ef952afccdbdf4367b0731e4
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2024-01-06 16:39:38 +0000

    sets sheet 6

--- FormalisingMathematics2024/Section04sets/Sheet6.lean
+++ FormalisingMathematics2024/Section04sets/Sheet6.lean
@@ -3,9 +3,8 @@ Copyright (c) 2022 Kevin Buzzard. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Author : Kevin Buzzard
 -/
-import Mathlib.Tactic.Default
-import Data.Real.Basic
 
+import Mathlib.Tactic -- imports all the Lean tactics
 
 /-!
 
@@ -13,37 +12,30 @@ import Data.Real.Basic
 
 ## Pushforward of a set along a map
 
-If `f : X → Y` then given a subset `S : set X` of `X` we can push it
-forward to make a subset `f(S) : set Y` of `Y`. The definition
-of `f(S)` is `{y : Y | ∃ x : X, x ∈ S ∧ f x = y}`. 
+If `f : X → Y` then given a subset `S : Set X` of `X` we can push it
+forward along `f` to make a subset `f(S) : Set Y` of `Y`. The definition
+of `f(S)` is `{y : Y | ∃ x : X, x ∈ S ∧ f x = y}`.
 
 However `f(S)` doesn't make sense in Lean, because `f` eats
-terms of type `X` and not `S`, which has type `set X`. 
+terms of type `X` and not `S`, which has type `Set X`.
 In Lean we use the notation `f '' S` for this. This is notation
-for `set.image` and if you need any API for this, it's likely
+for `Set.image` and if you need any API for this, it's likely
 to use the word `image`.
 
 ## Pullback of a set along a map
 
-If `f : X → Y` then given a subset `T : set Y` of `Y` we can
-pull it back to make a subset `f⁻¹(T) : set X` of `X`. The definition
-of `f⁻¹(T)` is `{x : X | f x ∈ T}`.
+If `f : X → Y` then given a subset `T : Set Y` of `Y` we can
+pull it back along `f` to make a subset `f⁻¹(T) : Set X` of `X`. The
+definition of `f⁻¹(T)` is `{x : X | f x ∈ T}`.
 
 However `f⁻¹(T)` doesn't make sense in Lean either, because
-`⁻¹` is notation for `has_inv.inv`, whose type in Lean
+`⁻¹` is notation for `Inv.inv`, whose type in Lean
 is `α → α`. In other words, if `x` has a certain type, then
-`x⁻¹` *must* have the same type. The notation was basically designed
+`x⁻¹` *must* have the same type: the notation was basically designed
 for group theory. In Lean we use the notation `f ⁻¹' T` for this pullback.
-This is notation for `set.preimage` and if you need any API for this,
-it's likely to use the word `preimage`.
 
 -/
 
-
--- imports all the Lean tactics
--- imports all the Lean tactics
--- the reals
--- the reals
 variable (X Y : Type) (f : X → Y) (S : Set X) (T : Set Y)
 
 example : S ⊆ f ⁻¹' (f '' S) := by sorry
@@ -68,4 +60,3 @@ example : g ∘ f ⁻¹' U = f ⁻¹' (g ⁻¹' U) := by sorry
 
 -- preimage of preimage is preimage of comp
 example : g ∘ f '' S = g '' (f '' S) := by sorry
-
