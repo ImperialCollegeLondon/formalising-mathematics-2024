9f1b562f23911fa3e883c4ed3bcf97e859937af4
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2024-01-06 16:49:59 +0000

    groups sheet 2

--- FormalisingMathematics2024/Section05groups/Sheet2.lean
+++ FormalisingMathematics2024/Section05groups/Sheet2.lean
@@ -3,20 +3,15 @@ Copyright (c) 2023 Kevin Buzzard. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Author : Kevin Buzzard
 -/
-import Mathlib.Tactic.Default
-
-
--- imports all the Lean tactics
--- imports all the Lean tactics
-namespace Section4sheet2
+import Mathlib.Tactic -- imports all the tactics
 
 /-!
 
 # Challenge sheet
 
-This is a harder group theory question. 
+This is a harder group theory question.
 
-It turns out that two of the axioms in our definition of a group
+It turns out that two of the axioms in the standard definition of a group
 are not needed; they can be deduced from the others. Let's define
 a "weak group" class, where we only have three of the group axioms.
 The question is: can you prove that a weak group is a group, by
@@ -34,16 +29,17 @@ class WeakGroup (G : Type) extends One G, Mul G, Inv G : Type where
 
 namespace WeakGroup
 
+-- Let `G` be a "weak group" and say a,b,c are in G
 variable {G : Type} [WeakGroup G] (a b c : G)
 
 /-
 
-The challenge is to prove that G is a group, which we can interpret as
+The (tricky) challenge is to prove that G is a group, which we can interpret as
 proving the missing axioms `mul_one` and `mul_inv_self`. Note that you
 can't use the `group` tactic any more because `G` isn't a group yet:
 this is what you're trying to prove!
 
-One way of doing it: try proving 
+One way of doing it: try proving
 
 `mul_left_cancel : a * b = a * c → b = c`
 
@@ -54,23 +50,26 @@ and then
 first.
 
 -/
-theorem hMul_left_cancel (h : a * b = a * c) : b = c := by sorry
 
-theorem hMul_one (a : G) : a * 1 = a := by sorry
+theorem mul_left_cancel (h : a * b = a * c) : b = c := by sorry
 
-theorem hMul_inv_self (a : G) : a * a⁻¹ = 1 := by sorry
+theorem mul_eq_of_eq_inv_mul (h : b = a⁻¹ * c) : a * b = c := by sorry
+
+theorem mul_one (a : G) : a * 1 = a := by sorry
+
+theorem mul_inv_self (a : G) : a * a⁻¹ = 1 := by sorry
 
 end WeakGroup
 
 /-
 
 If you want to take this further: prove that if we make
-a new class `bad_group` by replacing
+a new class `BadGroup` by replacing
 `one_mul` by `mul_one` in the definition of `weak_group`
-then it is no longer true that you can prove `one_mul`;
+then it is no longer true that you can prove `mul_inv_self`;
 there are structures which satisfy `mul_assoc`, `mul_one`
 and `inv_mul_self` but which really are not groups.
-Can you find an example? Try it on paper first. 
+Can you find an example? Try it on paper first.
 
 -/
 -- claim: not a group in general
@@ -79,7 +78,7 @@ class BadGroup (G : Type) extends One G, Mul G, Inv G : Type where
   mul_one : ∀ a : G, a * 1 = a
   inv_mul_self : ∀ a : G, a⁻¹ * a = 1
 
--- `bool` is a type with two terms, `bool.tt` and `bool.ff`. See if you can make it into 
+-- `Bool` is a type with two terms, `Bool.true` and `Bool.false`. See if you can make it into
 -- a bad group which isn't a group!
 instance : One Bool :=
   ⟨sorry⟩
@@ -92,14 +91,11 @@ instance : Inv Bool :=
 
 instance : BadGroup Bool where
   mul_assoc := sorry
-  -- `dec_trivial`, might be able to do this
+  -- `decide`, might be able to do this
   mul_one := sorry
-  -- dec_trivial,
+  -- decide
   inv_mul_self := sorry
+  -- decide
 
---dec_trivial,
 example : ¬∀ a : Bool, 1 * a = a := by sorry
-
---dec_trivial,
-end Section4sheet2
-
+-- decide
