c7a30d0f8b476a3e000d5034eac3eb5023099c4b
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2024-01-11 19:20:27 +0000

    add Lean Copilot!

--- lakefile.lean
+++ lakefile.lean
@@ -3,7 +3,11 @@ open Lake DSL
 
 package «formalising-mathematics-2024» {
   -- add any package configuration options here
+  -- this for Lean Copilot
+  moreLinkArgs := #["-L./.lake/packages/LeanCopilot/.lake/build/lib", "-lctranslate2"]
 }
+
+require LeanCopilot from git "https://github.com/lean-dojo/LeanCopilot.git" @ "v1.0.1"
 
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"
