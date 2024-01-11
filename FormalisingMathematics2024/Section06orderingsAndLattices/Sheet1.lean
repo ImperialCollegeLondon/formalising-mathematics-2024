2c287e3bea35167976d824d38d23f6200f8648ff
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2024-01-11 10:19:14 +0000

    sect 6 sheet 1

--- FormalisingMathematics2024/Section06orderingsAndLattices/Sheet1.lean
+++ FormalisingMathematics2024/Section06orderingsAndLattices/Sheet1.lean
@@ -3,25 +3,26 @@ Copyright (c) 2023 Kevin Buzzard. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Author : Kevin Buzzard
 -/
-import Mathlib.Tactic.Default
-
+import Mathlib.Tactic
 
 /-
 
 ## Orderings and lattices
 
-In the last section we saw how subsets of a type worked, and we saw that 
+In section 4 we saw how subsets of a type worked, and we saw that
 things like `⊆` and `∪` and `∩` made sense for subsets, and they satisfied
-theorems such as `A ∩ B ⊆ B`. But it turns out that there is a more general
-abstraction called a *lattice* which captures these kinds of ideas, and I'd
-like to explain this concept in this section. Note that the word "lattice" 
-unfortunately means several things in mathematics; this is the use of the
+theorems such as `A ∩ B ⊆ B`.
+
+But it turns out that there is a more general abstraction called a *lattice*
+which captures these kinds of ideas, and I'd like to explain this
+concept in this section. Note that the word "lattice" unfortunately
+means several distinct things in mathematics; this is the use of the
 word in the context of partial orders. So let me start by talking about
 partial orders.
 
 ## Partial orders
 
-A partial order (or a partially ordered type) is a type `X` equipped with
+A *partial order* (or a partially ordered type) is a type `X` equipped with
 a concept of `≤` satisying some axioms. More precisely `X` is equipped
 with a true-false statement `a ≤ b` for each `a b : X`, satisfying
 the following axioms:
@@ -33,44 +34,11 @@ the following axioms:
 Examples of partial orders include the natural numbers and the real numbers. However
 these examples are not quite representative, because a partial order does *not* have
 the axiom that for all `a b : X` we have either `a ≤ b` or `b ≤ a`. A perhaps more
-representative example of a partial order is the type `set X` of subsets of a type `X`,
+representative example of a partial order is the type `Set X` of subsets of a type `X`,
 with `a ≤ b` defined to mean `a ⊆ b`. For two general subsets `a` and `b` of `X`,
-both `a ⊆ b` and `b ⊆ a` might be false. 
-
+`a ⊆ b` and `b ⊆ a` might both be false.
 -/
-/-
 
-## Orderings and lattices
-
-In the last section we saw how subsets of a type worked, and we saw that 
-things like `⊆` and `∪` and `∩` made sense for subsets, and they satisfied
-theorems such as `A ∩ B ⊆ B`. But it turns out that there is a more general
-abstraction called a *lattice* which captures these kinds of ideas, and I'd
-like to explain this concept in this section. Note that the word "lattice" 
-unfortunately means several things in mathematics; this is the use of the
-word in the context of partial orders. So let me start by talking about
-partial orders.
-
-## Partial orders
-
-A partial order (or a partially ordered type) is a type `X` equipped with
-a concept of `≤` satisying some axioms. More precisely `X` is equipped
-with a true-false statement `a ≤ b` for each `a b : X`, satisfying
-the following axioms:
-
-`le_refl a : a ≤ a`
-`le_antisymm : a ≤ b → b ≤ a → a = b`
-`le_trans : a ≤ b → b ≤ c → a ≤ c`
-
-Examples of partial orders include the natural numbers and the real numbers. However
-these examples are not quite representative, because a partial order does *not* have
-the axiom that for all `a b : X` we have either `a ≤ b` or `b ≤ a`. A perhaps more
-representative example of a partial order is the type `set X` of subsets of a type `X`,
-with `a ≤ b` defined to mean `a ⊆ b`. For two general subsets `a` and `b` of `X`,
-both `a ⊆ b` and `b ⊆ a` might be false. 
-
--/
--- Let `X` be a partial order.
 -- Let `X` be a partial order.
 variable (X : Type) [PartialOrder X]
 
@@ -78,9 +46,8 @@ variable (X : Type) [PartialOrder X]
 example (a b c : X) (hab : a ≤ b) (hbc : b ≤ c) : a ≤ c :=
   le_trans hab hbc
 
--- or you can use the `transitivity` tactic
-example (a b c : X) (hab : a ≤ b) (hbc : b ≤ c) : a ≤ c :=
-  by
+-- or you can use the `trans` tactic
+example (a b c : X) (hab : a ≤ b) (hbc : b ≤ c) : a ≤ c := by
   trans b
   · exact hab
   · exact hbc
@@ -89,9 +56,11 @@ example (a b c : X) (hab : a ≤ b) (hbc : b ≤ c) : a ≤ c :=
 variable (a b c d : X)
 
 -- See if you can prove these basic facts about partial orders.
-example : a ≤ a := by sorry
+example : a ≤ a := by
+  sorry
 
-example (hab : a ≤ b) (hbc : b ≤ c) (hcd : c ≤ d) : a ≤ d := by sorry
+example (hab : a ≤ b) (hbc : b ≤ c) (hcd : c ≤ d) : a ≤ d := by
+  sorry
 
-example (hab : a ≤ b) (hbc : b ≤ c) (hca : c ≤ a) : a = b := by sorry
-
+example (hab : a ≤ b) (hbc : b ≤ c) (hca : c ≤ a) : a = b := by
+  sorry
