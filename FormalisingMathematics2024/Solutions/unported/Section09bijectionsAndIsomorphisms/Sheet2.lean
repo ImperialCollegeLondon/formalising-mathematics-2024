d6c2f534879ab61a4573b43dd2690bce0113a760
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2024-01-16 15:36:57 +0000

    sheet 2 done

--- FormalisingMathematics2024/Solutions/unported/Section09bijectionsAndIsomorphisms/Sheet2.lean
+++ FormalisingMathematics2024/Solutions/unported/Section09bijectionsAndIsomorphisms/Sheet2.lean
@@ -3,71 +3,41 @@ Copyright (c) 2023 Kevin Buzzard. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Author : Kevin Buzzard
 -/
-import Mathlib.Tactic.Default
+import Mathlib.Tactic
 
 
 /-
 
 # Constructive bijections
 
-I'm generally quite anti-constructive mathematics; it makes stuff harder
-to do in Lean whilst only providing benefits such as computational content
-which I am typically not interested in (I never `#eval` stuff, I just
-want to prove theorems and I don't care if the proof isn't `refl`).
+I'm generally quite anti constructive mathematics; it makes things harder
+and more confusing for beginners, whilst typically only providing benefits such as
+computational content which I am typically not interested in (I never `#eval` stuff,
+I just want to prove theorems and I don't care if the proof isn't `rfl`).
 
 But one example of where I love constructivism is `X ≃ Y`, the class
 of constructive bijections from `X` to `Y`. What is a constructive
 bijection? It is a function `f : X → Y` plus some more data, but here
 the data is *not* just the propositional claim that `f` is bijective
 (i.e. the *existence* of a two-sided inverse) -- it is the actual
-data of the two-sided inverse too. 
+data of the two-sided inverse too.
 
-`X ≃ Y` is notation for `equiv X Y`. This is the type of constructive
+`X ≃ Y` is notation for `Equiv X Y`. This is the type of constructive
 bijections from `X` to `Y`. To make a term of type `X ≃ Y` you need
 to give a 4-tuple consisting of two functions `f : X → Y` and `g : Y → X`,
 plus also two proofs: firstly a proof of `∀ y, f (g y) = y`, and secondly
 a proof of `∀ x, g (f x) = x`.
 
-Note that `X ≃ Y` has type `Type`, not `Prop`. It does *not* mean "there exists
-a bijection from `X` to `Y`", it is the actual data of a bijection
-from `X` to `Y`. 
+Note that `X ≃ Y` has type `Type`, not `Prop`. In other words, a term
+of type `X ≃ Y` is *not* a proof that there exists a bijection from `X` to `Y`,
+it is the actual data of a bijection from `X` to `Y`.
 
-Let's build two different bijections from ℚ to ℚ. 
-
-The first one is easy; I'll do it for you, to show you the syntax.
-
--/
-/-
-
-# Constructive bijections
-
-I'm generally quite anti-constructive mathematics; it makes stuff harder
-to do in Lean whilst only providing benefits such as computational content
-which I am typically not interested in (I never `#eval` stuff, I just
-want to prove theorems and I don't care if the proof isn't `refl`).
-
-But one example of where I love constructivism is `X ≃ Y`, the class
-of constructive bijections from `X` to `Y`. What is a constructive
-bijection? It is a function `f : X → Y` plus some more data, but here
-the data is *not* just the propositional claim that `f` is bijective
-(i.e. the *existence* of a two-sided inverse) -- it is the actual
-data of the two-sided inverse too. 
-
-`X ≃ Y` is notation for `equiv X Y`. This is the type of constructive
-bijections from `X` to `Y`. To make a term of type `X ≃ Y` you need
-to give a 4-tuple consisting of two functions `f : X → Y` and `g : Y → X`,
-plus also two proofs: firstly a proof of `∀ y, f (g y) = y`, and secondly
-a proof of `∀ x, g (f x) = x`.
-
-Note that `X ≃ Y` has type `Type`, not `Prop`. It does *not* mean "there exists
-a bijection from `X` to `Y`", it is the actual data of a bijection
-from `X` to `Y`. 
-
-Let's build two different bijections from ℚ to ℚ. 
+Let's build two different bijections from `ℚ` to `ℚ`.
 
 The first one is easy; I'll do it for you, to show you the syntax.
 
 -/
+
 def bijection1 : ℚ ≃ ℚ where
   toFun := id
   -- use the identity function from ℚ to ℚ
@@ -77,16 +47,15 @@ def bijection1 : ℚ ≃ ℚ where
     by
     -- we have to prove ∀ q, id (id q) = q
     intro q
-    rfl
+    rfl --- works because `id q` is definitionally equal to `q`.
+  -- Here's the same proof but in term mode
   right_inv q := rfl
 
--- same proof but in term mode
 -- Now see if you can do a harder one.
 def bijection2 : ℚ ≃ ℚ where
   toFun q := 3 * q + 4
   invFun r := (r - 4) / 3
-  left_inv :=
-    by
+  left_inv := by
     -- start with `intro r`, then use `dsimp` to tidy up the mess
     intro r
     dsimp
@@ -94,4 +63,3 @@ def bijection2 : ℚ ≃ ℚ where
   right_inv := by
     intro s
     ring
-
