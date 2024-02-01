fdc5534dca94fbf0c5a42a54d4f444a2ade2f032
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2024-02-01 11:26:12 +0000

    switch off autoImplicit

--- lakefile.lean
+++ lakefile.lean
@@ -1,9 +1,13 @@
 import Lake
 open Lake DSL
 
-package «formalising-mathematics-2024» {
-  -- add any package configuration options here
-}
+package «formalising-mathematics-2024» where
+  leanOptions := #[
+    ⟨`pp.unicode.fun, true⟩, -- pretty-prints `fun a ↦ b`
+    ⟨`pp.proofs.withType, false⟩,
+    ⟨`autoImplicit, false⟩,
+    ⟨`relaxedAutoImplicit, false⟩
+  ]
 
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"
